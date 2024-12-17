1449b37942cfc0d06e5a625e5daece1c64389c18
Roma <rkproject.info@gmail.com>
2024-12-17 22:17:43 +0300

    system prompt file

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
 import TelegramBot from 'node-telegram-bot-api';
-import {TELEGRAM_TOKEN} from './config.js';
+import path from 'node:path';
+import fs from 'node:fs/promises';
 import {chatCreateItem, chatGetItem} from './chat.js';
 import {messageCreateItem, messageGetList, messageUpdateItem} from './message.js';
 import {assistantProcess} from './assistant.js';
+import {TELEGRAM_TOKEN} from './config.js';
 
 
-const bot = new TelegramBot(TELEGRAM_TOKEN, {polling: true});
-bot.on('message', onMessage);
+const bot = new TelegramBot(TELEGRAM_TOKEN, {
+  polling: false,
+  webHook: false,
+});
+
+let systemPrompt = '';
+
+////
+
+async function onPollingError(error) {
+  console.log('polling_error', error.code);
+}
 
 async function onMessage(msg) {
   if (!msg?.chat?.id || !msg?.text) {
@@ -30,10 +42,13 @@ async function onMessage(msg) {
   const messages = [
     {
       role: 'system',
-      content: 'Промпт',
+      content: systemPrompt,
     },
   ];
   for (const message of oldMessages) {
+    if (!(message.text && message.answer && !message.error)) {
+      continue;
+    }
     messages.push({
       role: 'user',
       content: message.text,
@@ -53,3 +68,17 @@ async function onMessage(msg) {
   await messageUpdateItem(chat._id, message.messageIndex, answer);
   await bot.sendMessage(chat._id, answer);
 }
+
+////
+
+async function bootstrap() {
+  systemPrompt = await fs.readFile(path.join(import.meta.dirname, 'prompt.md'), 'utf8');
+
+  bot.on('polling_error', onPollingError);
+  bot.on('message', onMessage);
+  await bot.startPolling({restart: true});
+}
+
+bootstrap()
+  .then((res) => res && console.log(res))
+  .catch((err) => err && console.log(err));
