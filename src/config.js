b7dd7151a489354f65030e6671774f3b2b6c75af
Roma <rkproject.info@gmail.com>
2024-12-23 19:03:24 +0300

    add mongo configuration

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
 export const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN || '';
 export const VSEGPT_TOKEN = process.env.VSEGPT_TOKEN || '';
+export const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017';
+export const MONGO_DB = process.env.MONGO_DB || 'travel-bot';
