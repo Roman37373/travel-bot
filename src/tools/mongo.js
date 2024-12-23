0e5fa7d48f31fba213f061b55a092107f2c9c1c8
Roma <rkproject.info@gmail.com>
2024-12-23 19:04:56 +0300

    add mongo driver

---
+++ src/tools/mongo.js
@@ -0,0 +1,34 @@
+import {MongoClient} from 'mongodb';
+import {MONGO_DB, MONGO_URL} from '../config.js'; // Файл конфигурации
+
+if(!MONGO_DB) {
+  throw new Error('MONGO_DB is required');
+}
+
+if(!MONGO_URL) {
+  throw new Error('MONGO_URL is required');
+}
+
+const mongo = new MongoClient(MONGO_URL);
+let db;
+
+export async function initMongo() {
+  await mongo.connect();
+  db = mongo.db(MONGO_DB);
+}
+
+export async function mongoFindOne(collection, filter = {}) {
+  return await db.collection(collection).findOne(filter);
+}
+
+export async function mongoInsertOne(collection, $set) {
+  return await db.collection(collection).insertOne($set);
+}
+
+export async function mongoUpdateOne(collection, filter = {}, $set) {
+  return await db.collection(collection).updateOne(filter, {$set});
+}
+
+export async function mongoGetList(collection, filter = {}, sort = {}, limit) {
+  return await db.collection(collection).find(filter).sort(sort).limit(limit).toArray();
+}
